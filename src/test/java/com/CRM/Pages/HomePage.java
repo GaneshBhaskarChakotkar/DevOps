932a4e6eeda24734fe220aa5d74762d4e3ab55a7
GaneshBhaskarChakotkar <ganeshchakotkar19@gmail.com>
2024-05-30 08:16:47 -0700

    injoy

--- src/test/java/com/CRM/Pages/HomePage.java
+++ src/test/java/com/CRM/Pages/HomePage.java
@@ -1,5 +1,28 @@
 package com.CRM.Pages;
 
-public class HomePage {
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 
-}
+public class HomePage {
+		
+		WebDriver POMdriver;
+		public HomePage (WebDriver d){
+			 this.POMdriver = d; 
+		 }
+
+		public void enterUserNameTextBox(String un) {
+			POMdriver.findElement(By.id("username")).sendKeys(un);	
+		}
+		
+		public void enterPassWordTextBox(String ps) {
+			POMdriver.findElement(By.id("password")).sendKeys(ps);
+		}
+		
+		public void clickOnLoginBtn() {
+			POMdriver.findElement(By.id("login")).click();	
+			}
+		
+	}
+
+
+
--- src/test/java/com/CRM/TestCases/HomePageTest.java
+++ src/test/java/com/CRM/TestCases/HomePageTest.java
@@ -1,5 +1,38 @@
 package com.CRM.TestCases;
 
-public class HomePageTest {
+import org.testng.annotations.Test;
 
+import com.CRM.Pages.HomePage;
+
+import java.time.Duration;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+public class HomePageTest {
+	WebDriver driver;
+	HomePage o1 ;
+  @Test
+  public void login() throws InterruptedException {
+	  o1.enterUserNameTextBox("GaneshDon");
+	  o1.enterPassWordTextBox("pass@123");
+	  o1.clickOnLoginBtn();
+	  Thread.sleep(3000);
+	  System.out.println(driver.getTitle());
+  }
+  @BeforeTest
+  public void beforeTest() {
+	  	driver = new EdgeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		driver.get("http://adactinhotelapp.com/");
+		o1 = new HomePage(driver);
+  }
+  @AfterTest
+  public void afterTest() {
+	  driver.close();
+	}	
 }
+
+
